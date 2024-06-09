6e4877043698797161d8453c1a1e820835919f8e
Serhii Demianov <gummadisonxgfdop9@gmail.com.serhiidemianov>
2024-06-09 10:20:57 +0900

    combobox for network added

--- src/components/page/Restaking.jsx
+++ src/components/page/Restaking.jsx
@@ -9,7 +9,6 @@ import ListToken from '../ListToken/ListToken';
 const items = [
     { value: 'pool1', label: 'Ethereum' },
     { value: 'pool2', label: 'Arbitrum' },
-    { value: 'pool3', label: 'BTC' },
     { value: 'pool4', label: 'Mantle' },
     { value: 'pool5', label: 'K2' },
 ];
@@ -58,7 +57,7 @@ const Restaking = () => {
         <div className="flex flex-col items-center mt-20 w-full gap-12">
             <Title prop={{ title: 'Restaking', TVL: '998,855,562', contents: xpearned }} />
             <div className="w-full lg:w-12/12 mt-16 mb-5">
-                {/* <ChainCombo items={items} /> */}
+                <ChainCombo items={items} />
                 {stablemessage}
                 {lsmessage}
                 {rsmessage}
