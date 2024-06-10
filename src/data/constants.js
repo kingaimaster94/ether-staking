e3c6f28e0f30323f5d636763d853cab48f0fa72e
kingaimaster94 <kingaimaster94@gmail.com>
2024-06-11 05:11:18 +0900

    add

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,9 +1,9 @@
-export const vaultManagerAddress = `0xf41B1B9651264b6cB9462f1A488a87601E8cDb10`;
-export const delegationManagerAddress = `0x04991B0782EEa70e4e81e22944794b9eA1Ce8ed2`;
-export const monitorAddress = `0x8b161E8f9Ae89b70A0240e9EDAE0c9E2a689508E`;
-export const limiterAddress = `0xD892dE56977F5381dE4Ce9596996D75Ce9C75944`;
+export const vaultManagerAddress = `0xb17987731e6454044e44f4808932180D31039CC6`;
+export const delegationManagerAddress = `0xa221eEC6c1f88f563b48C6237e518a0a00d86825`;
+export const monitorAddress = `0x2D1f45085832069934B9242Be17609f629369118`;
+export const limiterAddress = `0x5D8557E35D1fC3bda7cc6768dE4a2D1e04B8ce64`;
 export const adminAddress = `0x80ACec8539C7c4Bd6FcA6391c69C4AE80C8ce0eF`;
 
-export const vaultImplAddress = `0x40e0ae933A29B7e6e0911371E45755861B463f28`;
-export const vaultManagerImplAddress = `0x902D664fd7cF73B460901e27dC186A5CbAEE9A46`;
-export const delegationManagerImplAddress = `0x628de9fC9f16C1D3053B62f9630571aa0190E93d`;
+export const vaultImplAddress = `0xecdED3f7678aa3d584286BaBF8f82a99a5980A48`;
+export const vaultManagerImplAddress = `0xBdF68DCc65d630D1760C405FE1a9BC897e0d04Bd`;
+export const delegationManagerImplAddress = `0xc3E645821eE6059e14f6D29df6d981e7b54C0376`;
